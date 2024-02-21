0be0251b75e1dca404609a54b6e15a57ee26e1ae
altamisatmaja <altamisfattah16@gmail.com>
2024-02-21 08:22:46 +0700

    feat: init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./views/**/*.blade.php",
+    "./views/**/*.js",
+    "./views/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../public/css/output.css" rel="stylesheet">
+</head>
+
+<body>
+    <p class="text-3xl font-bold underline text-blue-400">
+        apa lu liat-liat
+    </h>
+</body>
+
+</html>
